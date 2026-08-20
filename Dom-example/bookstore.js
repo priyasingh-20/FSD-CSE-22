c7e8eb33829bd154ae1c45beb10350b5430184db
Priya <priyasingh030912@gmail.com>
2026-08-20 16:04:09 +0530

    Error correction

--- Dom-example/bookstore.js
+++ Dom-example/bookstore.js
@@ -1,17 +1,17 @@
 const bookdata=[
-    {image:"",price:473},
-    {image:"",price:473},
-    {image:"",price:574},
+    {image:"https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQvKsjiZ5OoUFQQNbd8QxQUJrfKcrlFIhrEQtl_GlZN4jWW-5igk9uI6LHJvzz0numcGUllNKB6zMqzGThwgZzNf0KLjD1vCAQG8DGT60UqNcXOrjcLcrDXHcPAWWo07LNhUZYAHrkRhlM&usqp=CAc",price:473},
+    {image:"https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQvKsjiZ5OoUFQQNbd8QxQUJrfKcrlFIhrEQtl_GlZN4jWW-5igk9uI6LHJvzz0numcGUllNKB6zMqzGThwgZzNf0KLjD1vCAQG8DGT60UqNcXOrjcLcrDXHcPAWWo07LNhUZYAHrkRhlM&usqp=CAc",price:473},
+    {image:"https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQvKsjiZ5OoUFQQNbd8QxQUJrfKcrlFIhrEQtl_GlZN4jWW-5igk9uI6LHJvzz0numcGUllNKB6zMqzGThwgZzNf0KLjD1vCAQG8DGT60UqNcXOrjcLcrDXHcPAWWo07LNhUZYAHrkRhlM&usqp=CAc",price:574},
 ]
-function Book(){
+function Book(b){
 const div=document.createElement("div");
 div.setAttribute("class","book");
 const image=document.createElement("img");
-image.setAttribute("src",bookdata.image);
+image.setAttribute("src",b.image);
 image.setAttribute("height","50px");
 image.setAttribute("width","50px");
 const h2=document.createElement("h2");
-h2.innerText="Price:"+bookdata.price+"/-";;
+h2.innerText="Price:"+b.price+"/-";;
 h2.style.color="blue";
 const bt=document.createElement("button");
 bt.innerText="AddToCart";
